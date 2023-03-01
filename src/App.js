9d5756e4144c19a8fdc315ec039ada6dacb63673
kulandaiyesum <kulandaiyesu333@gmail.com>
2023-03-01 12:40:10 +0530

    to host

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=ee1c43b1";
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=ee1c43b1";
 
 // const movie1 = {
 //     "Title": "The Amazing Spiderman 2 Webb Cut",
